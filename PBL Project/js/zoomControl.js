b2aa174689d03d978e12365cb9aa04322ab89ccd
asd <105500376+Rrrrrrasd@users.noreply.github.com>
2024-10-10 19:56:07 +0900

    Delete PBL Project directory
    delete old folder

--- PBL Project/js/zoomControl.js
+++
@@ -1,7 +0,0 @@
-// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
-var mapTypeControl = new kakao.maps.MapTypeControl();
-map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
-
-
-var zoomControl = new kakao.maps.ZoomControl();
-map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
